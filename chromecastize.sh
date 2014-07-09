f8de32635149a746fe5d472bf02903d7cee82dcb
dm <murzzz@gmail.com>
2014-07-09 18:24:36 +0200

    report errors, otherwise converter will fail silently

--- chromecastize.sh
+++ chromecastize.sh
@@ -155,7 +155,7 @@ process_file() {
 		mark_as_good "$FILENAME"
 	else
 		echo "- video length: `mediainfo --Inform="General;%Duration/String3%" "$FILENAME"`"
-		$FFMPEG -loglevel panic -stats -i "$FILENAME" -scodec copy -vcodec "$OUTPUT_VCODEC" -acodec "$OUTPUT_ACODEC" "$FILENAME.mkv" && on_success "$FILENAME" || on_failure "$FILENAME"
+		$FFMPEG -loglevel error -stats -i "$FILENAME" -scodec copy -vcodec "$OUTPUT_VCODEC" -acodec "$OUTPUT_ACODEC" "$FILENAME.mkv" && on_success "$FILENAME" || on_failure "$FILENAME"
 		echo ""
         fi
 }
